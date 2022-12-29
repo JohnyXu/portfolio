e74b323f568fa5a41dbb9651710bf10837414d8a
Johny Xu <ahstuxq@gmail.com>
2022-12-29 16:05:49 +1100

    update tags

--- src/container/Work/Work.tsx
+++ src/container/Work/Work.tsx
@@ -44,7 +44,7 @@ const Work = () => {
       </h2>
 
       <div className="app__work-filter">
-        {['Web App', 'Mobile App', 'React JS', 'All'].map((item: string, index) => (
+        {['Web', 'Mobile', 'React', 'All'].map((item: string, index) => (
           <div
             key={index}
             onClick={() => handleWorkFilter(item)}
